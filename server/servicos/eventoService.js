a183ccdfdc062ca53968ad79f6e217d3f3fec6ac
JeanPauloKehl <jeankehl@hotmail.com>
2020-10-03 10:56:47 -0300

    Tarefa #6 Adicionando mais um evento

--- server/servicos/eventoService.js
+++ server/servicos/eventoService.js
@@ -1,14 +1,14 @@
 const logger = require('../util/logger.js');
 
-module.exports = function(app) {
+module.exports = function (app) {
   const connection = app.persistencia.connection;
   const userDAO = new app.dao.userDAO(connection);
 
 
-  app.get('/eventoService/adquirirEventos/', function(req, res) {
+  app.get('/eventoService/adquirirEventos/', function (req, res) {
     logger.info("Executando o método ==> usuarioService/efetuarLogin");
     const parameters = req.body;
-    res.send([{"image": "images/event.png","title": "Semana academica","mes": "SET","hour": "19:00","date": "17"}, {"image": "images/event.png","title": "CHINA E TOP","mes": "SET","hour": "19:00","date": "17"}]);
+    res.send([{ "image": "images/event.png", "title": "Apresentando para a Otilia", "mes": "NOV", "hour": "19:00", "date": "17" }, { "image": "images/event.png", "title": "Semana academica", "mes": "SET", "hour": "19:00", "date": "17" }, { "image": "images/event.png", "title": "CHINA E TOP", "mes": "SET", "hour": "19:00", "date": "17" }]);
   });
 
 }
